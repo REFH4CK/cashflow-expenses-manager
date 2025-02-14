57585217a29e65adc8f1c88824c298f82ce880a6
REFH4CK <refh4ck.dev@gmail.com>
2025-02-14 17:19:13 -0400

    add new path's

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import { Routes, Route } from "react-router";
 import { Landing } from "@/pages/Landing";
+import { Login } from '@/pages/App/Login/Login'
+import { Register } from "@/pages/App/Login/Register";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </>
   );
