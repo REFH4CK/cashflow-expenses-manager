5495e1295170c5e65f839c6f820f7ca24e3d5846
REFH4CK <refh4ck.dev@gmail.com>
2025-04-17 16:47:59 -0400

    config the server to manage uploaded image

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -1,20 +1,36 @@
 import express from "express";
 import cors from "cors";
+import fs from 'fs';
 import routes from "../routes/index.js";
 import cookieParser from "cookie-parser";
 import { redirectUrl } from "../middleware/redirectUrl.js";
 
 const app = express();
 
+const uploadDir = 'uploads/profile-photos';
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
+
+// Servir archivos estáticos
+app.use('/cashflow/api/profile-photos', express.static(uploadDir));
+
 app.use(cookieParser());
 app.use(cors({
-  origin: true,
+  origin: 'http://localhost:5173',
   credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 })); // Allow request of differents sites
 app.use(express.json()); // Parse the request body to JSON
 
 app.use(redirectUrl);
 
 app.use("/", routes);
+
+
+
+
+
 
 export default app;
