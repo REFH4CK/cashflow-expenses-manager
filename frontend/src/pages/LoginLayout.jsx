2daf99c407dae10e7a75d502850f6953bbe6f453
REFH4CK <refh4ck.dev@gmail.com>
2025-05-31 16:43:37 -0400

    adjust the bg

--- frontend/src/pages/LoginLayout.jsx
+++ frontend/src/pages/LoginLayout.jsx
@@ -1,9 +1,11 @@
+import loginBg from '@/assets/images/loginbg.png';
+
 export function LoginLayout({ children }) {
   return (
     <>
       <main
         className="min-h-screen bg-no-repeat bg-cover w-full h-[100dvh] flex justify-center items-center flex-col"
-        style={{ backgroundImage: "url('/src/assets/images/loginbg.png')" }}
+        style={{ backgroundImage: `url(${loginBg})` }}
       >
         {children}
       </main>
